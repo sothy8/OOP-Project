4677767c1236ad781a7a9f3460408ad5d3735b70
Sothy Vandeth <vandethsothy@Sothys-MacBook-Air.local>
2025-01-05 22:29:59 +0700

    Initial commit

--- product/models.py
+++ product/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
-    # Auto-incrementing primary key (default)
-    product_id = models.TextField(max_length=30)
-    # product_id = models.BigAutoField(primary_key=True)
+    # Auto-incrementing primary key
+    product_id = models.BigAutoField(primary_key=True)
 
     # Category Choices
     class Category(models.TextChoices):
@@ -22,7 +20,7 @@ class Product(models.Model):
         OTHER = ("OTHER", "OTHER")
 
     product_category = models.CharField(
-        max_length=50,  # Increased max_length to accommodate longer category names
+        max_length=50,
         choices=Category.choices,
         default=Category.OTHER,
     )
@@ -44,18 +42,13 @@ class Product(models.Model):
     )
     
     product_color = models.TextField(max_length=30)
-    
     product_brand = models.TextField(max_length=30)
-    
     product_price = models.DecimalField(
-        max_digits=10,  # Total number of digits (before the decimal point)
-        decimal_places=2,  # Number of digits after the decimal point (for cents)
+        max_digits=10,
+        decimal_places=2,
         default=0.00,
     )
-    
-    product_releasing_year = models.PositiveIntegerField(
-        default=2023  # Default year; you can update this logic as needed
-    )
+    product_releasing_year = models.PositiveIntegerField(default=2023)
     
     def __str__(self):
         return f"{self.product_id} - {self.product_item} | {self.product_category} | {self.product_size} | ${self.product_price:.2f}"
